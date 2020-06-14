522d52ada198ea428fe974cac3f906f08669e232
MaxMichel2 <maxime_michel_uk@hotmail.com>
2020-06-14 18:36:32 +0200

    Internationalization update

--- InternationalMobilityProject/internationalmobility/asgi.py
+++ InternationalMobilityProject/internationalmobility/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'internationalmobility.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'internationalmobility.settings.development')
 
 application = get_asgi_application()
